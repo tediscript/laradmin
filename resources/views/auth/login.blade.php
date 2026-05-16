e1ca87bb560f42f76ac20f71b8189da9efd7c9cc
Tedi Fibri <tediscript@gmail.com>
2026-05-16 13:43:37 +0700

    feat(auth): add register link to login page
    Adds 'Don\'t have an account?' link below the login form, mirroring
    the existing 'Already registered?' link on the register page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -98,6 +98,12 @@
                         {{ __('Log in') }}
                     </x-primary-button>
                 </div>
+
+                <div class="mt-4 text-center">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-700" href="{{ route('register') }}">
+                        {{ __("Don't have an account?") }}
+                    </a>
+                </div>
             </form>
         @endif
     @endif
